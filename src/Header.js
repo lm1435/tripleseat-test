41dbafc854693826fcdbd53f2681f907bc77e681
Luis Meza <luismeza01@gmail.com>
2024-05-28 13:05:08 -0600

    adding npm changes

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Col, Row, Container } from 'reactstrap';
+import React from "react";
+import styled from "styled-components";
+import { Col, Row, Container } from "reactstrap";
 
 const Wrapper = styled.div`
   margin: 1em auto;
-  text-align: center; 
-`
+  text-align: center;
+`;
 
 const Paragraph = styled.p`
   color: #9ad6da;
   font-size: 2em;
   margin: auto;
-`
+`;
 
 const Input = styled.input`
-  border: 2px solid #e87e81
-`
+  border: 2px solid #e87e81;
+`;
 
 const header = {
   color: "#e87e81",
   fontWeight: "bold",
-  margin: "10px 0"
-}
+  margin: "10px 0",
+};
 
 const Header = ({ children, searchTerm, onSearchChange }) => {
   return (
@@ -29,16 +29,15 @@ const Header = ({ children, searchTerm, onSearchChange }) => {
       <Container>
         <Row>
           <Col xs="12" md="6" style={header}>
-            <img src="https://www.tripleseat.com/wp-content/themes/tripleseat_wordpress/images/logo.png" alt="tripleseat logo"/>
+            <img
+              src="https://cdn.brightanalytics.eu/wp-content/uploads/Tripleseat-logo-off.png"
+              alt="tripleseat logo"
+            />
           </Col>
           <Col xs="12" md="6" style={header}>
             <form>
               <span>Search: </span>
-              <Input
-                type="text"
-                value={searchTerm}
-                onChange={onSearchChange}
-              />
+              <Input type="text" value={searchTerm} onChange={onSearchChange} />
             </form>
           </Col>
           <Paragraph>{children}</Paragraph>
@@ -46,6 +45,6 @@ const Header = ({ children, searchTerm, onSearchChange }) => {
       </Container>
     </Wrapper>
   );
-}
+};
 
 export default Header;
